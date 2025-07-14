677eeae79eaed16a67cffa7b40de363ec60bb510
bhargavi374 <nareddybhargavi47@gmail.com>
2025-07-14 12:05:16 +0530

    studentprofile done

--- src/Pages/Profilepage.jsx
+++ src/Pages/Profilepage.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import React from 'react';
+import { useState } from 'react';
+import StudentTb from '../components/StudentTb/StudentTb.jsx';
+import StudentDetails from '../components/StudentDetails/StudentDetails.jsx';
+// import '../styles/Profilepage.css';
+import '../index.css'
 const Profilepage = () => {
+  const [selectedStudent, setselectedStudent] = useState(null);
   return (
-    <div>Profilepage</div>
+    <div className='student-profile-page'>
+      <h1>Student Profiles</h1>
+      <div className="profile-content">
+        <StudentTb onSelectStudent={setselectedStudent} />
+        {selectedStudent ? (
+          <StudentDetails student={selectedStudent} />
+        ) : (
+          <div className="placeholder">
+            Select a student to view details
+          </div>
+        )}
+      </div>
+    </div>
   )
-}
+};
 
-export default Profilepage
+export default Profilepage;
